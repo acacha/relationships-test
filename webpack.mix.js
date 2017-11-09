5da0d67d9f4f6716cd798fbddc058393a4a57014
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-11-09 11:26:34 +0100

    Sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,7 +17,7 @@ mix.js('resources/assets/js/app.js', 'public/js')
   .webpackConfig({
     resolve: {
       modules: [
-        path.resolve(__dirname, '../relationships/resources/assets/js'),
+        path.resolve(__dirname, './relationships/resources/assets/js'),
         path.resolve(__dirname, 'node_modules')
       ]
     }
